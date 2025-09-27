83bc18c5a4beb9fd15371d3461da604e93437b29
Lalithya15 <lalithyavelamati@gmail.com>
2025-09-27 19:28:39 +0530

    Delete devops-intern directory

--- devops-intern/check_encoders.py
+++
@@ -1,7 +0,0 @@
-import joblib
-
-# Load the label encoders
-label_encoders = joblib.load("label_encoders.pkl")
-
-# Print all available keys
-print("Columns stored in label_encoders:", list(label_encoders.keys()))
